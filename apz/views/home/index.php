b0fd1a1ca8379e8a7a732e81082d03d3ce780a2a
fannyhasbi <fnyhsbi@gmail.com>
2018-06-25 03:44:06 +0700

    Add section to propose a project

--- apz/views/home/index.php
+++ apz/views/home/index.php
@@ -155,6 +155,21 @@ function bulan_definer($bulan){
       </div>
     </section>
 
+    <!-- Propose Section -->
+    <section id="propose" class="success">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h3 class="propose-title">Ajukan Proyek</h3>
+            <p class="propose-text">Ajukan sebuah proyek aplikasi anda kami.</p>
+          </div>
+          <div class="col-md-4 propose-btn-container">
+            <a class="btn btn-primary btn-lg" href="<?= site_url('pengajuan'); ?>">AJUKAN SEKARANG</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!-- Event Section -->
     <section id="event">
       <div class="container">
