46159cab519d40ea2207c971bd6b3585c85d8440
fannyhasbi <fnyhsbi@gmail.com>
2018-07-31 18:01:42 +0700

    Add edit button in club profile editor

--- apz/views/club/home_profile.php
+++ apz/views/club/home_profile.php
@@ -10,7 +10,7 @@
             <div class="col-md-6">
               <div class="form-group label-floating">
                 <label class="control-label">Nama Club</label>
-                <input type="text" class="form-control" name="nama" required value="<?= $club->nama; ?>">
+                <input type="text" class="form-control" name="nama" required value="<?= $club->nama; ?>" disabled readonly>
               </div>
             </div>
           </div>
@@ -19,15 +19,36 @@
             <div class="col-md-12">
               <div class="form-group label-floating">
                 <label class="control-label">Keterangan Biodata</label>
-                <textarea class="form-control" name="ket" required rows="6"><?= $club->ket; ?></textarea>
+                <textarea class="form-control" name="ket" required rows="6" readonly><?= $club->ket; ?></textarea>
               </div>
             </div>
           </div>
 
-          <input type="submit" id="btn" class="btn btn-primary pull-right" name="tambah" value="Tambah">
+          <input type="submit" id="btn" class="btn btn-primary" name="simpan" value="Simpan" disabled>
+          <button type="button" id="btn-edit" class="btn btn btn-default pull-right" value="not">Edit</button>
           <div class="clearfix"></div>
         </form>
       </div>
     </div>
   </div>
 </div>
+
+<script>
+$(function(){
+  $("#btn-edit").click(function(){
+    if($(this).val() == "not"){
+      $("textarea[name=ket]").attr("readonly", false);
+      $("#btn").attr("disabled", false);
+      $("textarea[name=ket]").focus();
+      $(this).val("edit");
+      $(this).html("Urungkan");
+    }
+    else {
+      $("textarea[name=ket]").attr("readonly", true);
+      $("#btn").attr("disabled", true);
+      $(this).val("not");
+      $(this).html("Edit");
+    }
+  });
+});
+</script>
