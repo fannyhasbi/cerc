45a1110668ec8a45696317f7baace011237012bb
fannyhasbi <fnyhsbi@gmail.com>
2018-08-09 18:32:40 +0700

    Display modal in post pictures

--- apz/views/club/profile.php
+++ apz/views/club/profile.php
@@ -18,18 +18,26 @@
     <h3 class="text-center">Our Activity</h3>
     <hr class="small">
 
-    <div class="row">
+    <div id="project" class="row">
       <?php if(count($post) == 0){ ?>
         <p>Belum ada aktivitas yang dibagikan.
       <?php } else { ?>
         <?php foreach($post as $item): ?>
-          <div class="col-xs-6 col-sm-3">
-            <?php $filter_url = filter_var($item->foto, FILTER_VALIDATE_URL); ?>
-            <img src="<?= $filter_url ? $item->foto : base_url('uploads/club/post/'.$item->foto); ?>" class="img-thumbnail">
+          <div class="col-sm-3 project-item">
+            <a class="project-link" href="#postModal-<?= $item->id; ?>" data-toggle="modal">
+              <div class="caption">
+                <div class="caption-content">
+                  <?= $item->judul; ?>
+                </div>
+              </div>
+              <?php $filter_url = filter_var($item->foto, FILTER_VALIDATE_URL); ?>
+              <img class="img-fluid" src="<?= $filter_url ? $item->foto : base_url('uploads/club/post/'.$item->foto); ?>">
+            </a>
           </div>
         <?php endforeach; ?>
       <?php } // end else ?>
     </div>
+
   </div>
 </section>
 
@@ -68,3 +76,47 @@
     </div>
   </div>
 </section>
+
+<?php foreach($post as $item): ?>
+  <div class="project-modal modal fade" id="postModal-<?= $item->id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <h2><?= $item->judul; ?></h2>
+                <hr class="small">
+                
+                <?php $filter_url = filter_var($item->foto, FILTER_VALIDATE_URL); ?>
+                <img class="img-fluid img-centered" src="<?= $filter_url ? $item->foto : base_url('uploads/club/post/'.$item->foto); ?>" alt="">
+                
+                <p><?= $item->ket == '' ? 'Keterangan belum disediakan.' : $item->ket ?></p>
+                <ul class="list-inline item-details">
+                  <li>Tanggal Kegiatan:
+                    <strong><?= tanggal_definer($item->tgl); ?></strong>
+                  </li>
+                  <li>Diupload pada:
+                    <strong><?= tanggal_definer($item->uploaded); ?></strong>
+                  </li>
+                  <li>Diupdate pada:
+                    <strong><?= $item->updated; ?></strong>
+                  </li>
+                </ul>
+                <button type="button" class="btn btn-success" data-dismiss="modal">
+                  <i class="fa fa-times"></i>
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php endforeach; ?>
